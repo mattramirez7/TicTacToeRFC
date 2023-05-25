014fb3e3f6169aac2801da5e4d2e8cb581cfecaa
Audrey Dennis <41072545+audreydennis@users.noreply.github.com>
2023-05-24 18:35:22 -0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -3,19 +3,32 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
+    String sessionID;
+    String clientIdentifier;
+    String symbol;
+    Boolean terminated;
+
     public static void main(String...args){
+        String host="localhost";
+        int port=3116;
+        String protocolVersion = "1";
+        terminated=false;
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("TCP or UDP: ");
         String service = scanner.nextLine();
 
-        System.out.print("Would u like to create a game or join an existing game?");
-        String action = scanner.nextLine();
+        System.out.print("Enter your Client Identifier: ");
+        String clientId = scanner.nextLine();
+
+        System.out.print("Enter Version: ");
+        String version = scanner.nextLine();
 
         if (service.equalsIgnoreCase("TCP")) {
             try {
             // Establish TCP connection
-            Socket socket = new Socket("localhost", 3116);
+            Socket socket = new Socket(host, port);
             System.out.println("TCP connection established");
 
             // Create input and output streams for the socket
@@ -23,14 +36,22 @@ public class Client {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
             // Send greeting message
-            String protocolVersion = "1";
-            String clientIdentifier = "CLIENT123"; // change this based on client name
-            out.println("HELO " + protocolVersion + " " + clientIdentifier);
-            System.out.println("Sent greeting message to the server.");
+            out.println("HELO " + version + " " + clientId);
+
+            // String acknowledgment = in.readLine();
+            // System.out.println("Received acknowledgment from the server: " + acknowledgment);
 
             // Receive acknowledgment
-            String acknowledgment = in.readLine();
-            System.out.println("Received acknowledgment from the server: " + acknowledgment);
+            while(!gameTermined){
+                String[] response=in.readLine().split(" ");
+                String message= handleResponse(response);
+                if(!gameTerminated){
+                    out.println(message);
+                }
+            }
+
+            // System.out.print("Would u like to create a game or join an existing game?");
+            // String action = scanner.nextLine();
 
             
 
@@ -46,6 +67,36 @@ public class Client {
         } else {
             System.out.println("Invalid service. Please choose TCP or UDP.");
         }
+
+        public static String handleResponse(String[] response){
+            String action=response[0];
+            String request="";
+            if(action.equals("SESS")){ //emily
+                request=getSession(response);
+            } else if (action.equals("BORD")){ // audrey
+                request=getJOHD(response);
+            } else if (action.equals("GAMS")){ // emily
+                request=getJOHD(response);
+            } else if (action.equals("GDBY")){ // audrey print goodbye close socket, update boolean
+                request=getJOHD(response);
+            } else if (action.equals("JOND")){ // emily
+                request=getJOHD(response);
+            } else if (action.equals("TERM")){ // audrey
+                request="TERM";
+            } else if (action.equals("YRMV")){ //emily
+                request=getJOHD(response);
+            }else{
+                System.out.println("method not supported");
+            }
+            return request;
+        }
+
+        public static String getSession(String[] response){
+
+
+        }
+
+    
     }
 
 
