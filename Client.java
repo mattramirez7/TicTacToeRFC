81de6f1303f6f4c208e962d78a5b082357d07f17
emilysmokoff <esmokoff@uw.edu>
2023-05-26 00:07:37 -0700

    Adding instancing

--- Client.java
+++ Client.java
@@ -3,17 +3,11 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
-    String sessionID;
-    String clientIdentifier;
-    String symbol;
-    Boolean terminated;
-    String board;
 
     public static void main(String...args){
         String host = "localhost";
         int port = 3116;
         String protocolVersion = "1";
-        terminated = false;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -21,7 +15,8 @@ public class Client {
         String service = scanner.nextLine();
 
         System.out.print("Enter your Client Identifier: ");
-        clientIdentifier = scanner.nextLine();
+        String clientIdentifier = scanner.nextLine();
+        ClientHandler newClient = new ClientHandler("", clientIdentifier, "", false, "|*|*|*|*|*|*|*|*|*|");
 
         System.out.print("Enter Version: ");
         String version = scanner.nextLine();
@@ -37,16 +32,16 @@ public class Client {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
             // Send greeting message
-            out.println("HELO " + version + " " + clientId);
+            out.println("HELO " + version + " " + newClient.getClientID());
 
             // String acknowledgment = in.readLine();
             // System.out.println("Received acknowledgment from the server: " + acknowledgment);
 
             // Receive acknowledgment
-            while(!gameTermined){
+            while(!newClient.getTerminated()){
                 String[] response=in.readLine().split(" ");
-                String message= handleResponse(response);
-                if(!gameTerminated && !message.equals("")){
+                String message= handleResponse(response, newClient);
+                if(!newClient.getTerminated() && !message.equals("")){
                     out.println(message + "\r\n");
                 } 
             }
@@ -68,106 +63,105 @@ public class Client {
         } else {
             System.out.println("Invalid service. Please choose TCP or UDP.");
         }
+    }
 
-        public static String handleResponse(String[] response){
-            String action = response[0];
-            String request = "";
-            if(action.equals("SESS")){ //emily make sure to prompt for creating, joining or looking at a list of open session ids
-                request = getSess(response);
-            } else if (action.equals("BORD")){ // audrey
-                request = getJOHD(response);
-            } else if (action.equals("GAMS")){ // emily
-                request = getGams(response);
-            } else if (action.equals("GDBY")){ // audrey print goodbye close socket, update boolean
-                request = getJOHD(response);
-            } else if (action.equals("JOND")){ // emily
-                request = getJond(response);
-            } else if (action.equals("TERM")){ // audrey
-                request = "TERM";
-            } else if (action.equals("YRMV")){ //emily
-                request = getYrmv(response);
-            }else{
-                System.out.println("method not supported");
-            }
-            return request;
+    public static String handleResponse(String[] response, ClientHandler newClient){
+        String action = response[0];
+        String request = "";
+        if(action.equals("SESS")){ //emily make sure to prompt for creating, joining or looking at a list of open session ids
+            request = getSess(response, newClient);
+        } else if (action.equals("BORD")){ // audrey
+            request = getJOHD(response, newClient);
+        } else if (action.equals("GAMS")){ // emily
+            request = getGams(response, newClient);
+        } else if (action.equals("GDBY")){ // audrey print goodbye close socket, update boolean
+            request = getJOHD(response, newClient);
+        } else if (action.equals("JOND")){ // emily
+            request = getJond(response, newClient);
+        } else if (action.equals("TERM")){ // audrey
+            request = "TERM";
+        } else if (action.equals("YRMV")){ //emily
+            request = getYrmv(response, newClient);
+        }else{
+            System.out.println("method not supported");
         }
+        return request;
+    }
 
-        public static String getSess(String[] response){
-            //response holds sess info passed from server SESS version uniquesessionid
-            sessionID = response[2];
+    public static String getSess(String[] response, ClientHandler newClient){
+        //response holds sess info passed from server SESS version uniquesessionid
+        newClient.setSessionID(response[2]);
 
-            //make sure to prompt for creating, joining or looking at a list of open session ids
-            Scanner scanner = new Scanner(System.in);
+        //make sure to prompt for creating, joining or looking at a list of open session ids
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Would you like to \"create\" a game, \"join\" a game, or view a \"list\" of open games?");
-            String beginGame = scanner.nextLine();
+        System.out.print("Would you like to \"create\" a game, \"join\" a game, or view a \"list\" of open games?");
+        String beginGame = scanner.nextLine();
 
-            if(beginGame.contains("create")) {
-                return "CREA " + clientIdentifier;
-            } else if(beginGame.contains("join")) {
-                System.out.print("Please enter the Game ID you'd like to join.");
-                String gameID = scanner.nextLine();
-                return "JOIN " + gameID;
-            } else if(beginGame.contains("list")) {
-                //add curr and all
-                System.out.print("Would you like to view all games \"currently\" open and in-play, or \"all\" games including those that have concluded?");
-                String listType = scanner.nextLine();
-
-                if(listType.contains("currently")) {
-                    return "LIST CURR";
-                } else if(listType.contains("all")) {
-                    return "LIST ALL";
-                }
-                return "";
-            } else {
-                return "";
-            }
-        }
-
-        public static String getGams(String[] response){
-            System.out.println("Here is a list of available games:");
-            for(int i = 1; i < response.length, i++) {
-                System.out.println(response[i]);
-            }
-
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Which game listed above would you like to join?");
+        if(beginGame.contains("create")) {
+            return "CREA " + newClient.getClientID();
+        } else if(beginGame.contains("join")) {
+            System.out.print("Please enter the Game ID you'd like to join.");
             String gameID = scanner.nextLine();
-
             return "JOIN " + gameID;
-        }
+        } else if(beginGame.contains("list")) {
+            //add curr and all
+            System.out.print("Would you like to view all games \"currently\" open and in-play, or \"all\" games including those that have concluded?");
+            String listType = scanner.nextLine();
 
-        public static String getJond(String[] response){
-            System.out.println("You have now joined the game: " + response[2]);
-            System.out.println("Please wait while we get the game started.");
-            //returns nothing because waiting for yrmv to be sent
+            if(listType.contains("currently")) {
+                return "LIST CURR";
+            } else if(listType.contains("all")) {
+                return "LIST ALL";
+            }
+            return "";
+        } else {
             return "";
         }
+    }
 
-        public static String getYrmv(String[] response){
-            Scanner scanner = new Scanner(System.in);
-
-            //your move, game id, client whose turn it is id
-            if(response[2].equals(clientIdentifier)) {
-                System.out.println(board);
-                System.out.print("It is your turn to make a move, which space would you like to occupy?");
-                String moveSpace = scanner.nextLine();
-
-                return "MOVE " + response[1] + " " + moveSpace;
-            } else {
-                System.out.println(board);
-                System.out.println("It is not your move, please wait for player: " + response[2] + " to go.");
-                return "";
-            }
+    public static String getGams(String[] response, ClientHandler newClient){
+        System.out.println("Here is a list of available games:");
+        for(int i = 1; i < response.length; i++) {
+            System.out.println(response[i]);
         }
 
-    
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Which game listed above would you like to join?");
+        String gameID = scanner.nextLine();
+
+        return "JOIN " + gameID;
+    }
+
+    public static String getJond(String[] response, ClientHandler newClient){
+        System.out.println("You have now joined the game: " + response[2]);
+        System.out.println("Please wait while we get the game started.");
+        //returns nothing because waiting for yrmv to be sent
+        return "";
+    }
+
+    public static String getYrmv(String[] response, ClientHandler newClient){
+        Scanner scanner = new Scanner(System.in);
+
+        //your move, game id, client whose turn it is id
+        if(response[2].equals(newClient.getClientID())) {
+            System.out.println(newClient.getBoard());
+            System.out.print("It is your turn to make a move, which space would you like to occupy?");
+            String moveSpace = scanner.nextLine();
+
+            return "MOVE " + response[1] + " " + moveSpace;
+        } else {
+            System.out.println(newClient.getBoard());
+            System.out.println("It is not your move, please wait for player: " + response[2] + " to go.");
+            return "";
+        }
     }
 
 
 
-    {/*
+
+    /*
 
     -Client is prompted for TCP or UDP
     -Ask for Self identifying name
@@ -191,5 +185,5 @@ public class Client {
     -loop back to do u want to start or join a game
 
     - create class to check for command 
-     */}
+     */
 }
---
+++ ClientHandler.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class ClientHandler {
+    private String sessionID;
+    private String clientIdentifier;
+    private String symbol;
+    private Boolean terminated;
+    private String board;
+
+    public ClientHandler (String sessionID, String clientIdentifier, String symbol, Boolean terminated, String board) {
+        this.sessionID = sessionID;
+        this.clientIdentifier = clientIdentifier;
+        this.symbol = symbol;
+        this.terminated = terminated;
+        this.board = board;
+    }
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public String getClientID() {
+        return clientIdentifier;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Boolean getTerminated() {
+        return terminated;
+    }
+
+    public String getBoard() {
+        return board;
+    }
+
+    public void setSessionID(String sessID) {
+        sessionID = sessID;
+    }
+
+    public void setClientID(String clientID) {
+        clientIdentifier = clientID;
+    }
+
+    public void setSymbol(String sym) {
+        symbol = sym;
+    }
+
+    public void setTerminated(Boolean term) {
+        terminated = term;
+    }
+
+    public void setBoard(String boardSent) {
+        board = boardSent;
+    }
+}
