a722ae92d934e94fae2aaab96e0ccbc8eb2e0146
emilysmokoff <esmokoff@uw.edu>
2023-05-25 23:20:19 -0700

    Adding functionality for sess, games, jond, and yrmv

--- Client.java
+++ Client.java
@@ -7,12 +7,13 @@ public class Client {
     String clientIdentifier;
     String symbol;
     Boolean terminated;
+    String board;
 
     public static void main(String...args){
-        String host="localhost";
-        int port=3116;
+        String host = "localhost";
+        int port = 3116;
         String protocolVersion = "1";
-        terminated=false;
+        terminated = false;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,7 +21,7 @@ public class Client {
         String service = scanner.nextLine();
 
         System.out.print("Enter your Client Identifier: ");
-        String clientId = scanner.nextLine();
+        clientIdentifier = scanner.nextLine();
 
         System.out.print("Enter Version: ");
         String version = scanner.nextLine();
@@ -69,31 +70,96 @@ public class Client {
         }
 
         public static String handleResponse(String[] response){
-            String action=response[0];
-            String request="";
+            String action = response[0];
+            String request = "";
             if(action.equals("SESS")){ //emily make sure to prompt for creating, joining or looking at a list of open session ids
-                request=getSession(response);
+                request = getSess(response);
             } else if (action.equals("BORD")){ // audrey
-                request=getJOHD(response);
+                request = getJOHD(response);
             } else if (action.equals("GAMS")){ // emily
-                request=getJOHD(response);
+                request = getGams(response);
             } else if (action.equals("GDBY")){ // audrey print goodbye close socket, update boolean
-                request=getJOHD(response);
+                request = getJOHD(response);
             } else if (action.equals("JOND")){ // emily
-                request=getJOHD(response);
+                request = getJond(response);
             } else if (action.equals("TERM")){ // audrey
-                request="TERM";
+                request = "TERM";
             } else if (action.equals("YRMV")){ //emily
-                request=getJOHD(response);
+                request = getYrmv(response);
             }else{
                 System.out.println("method not supported");
             }
             return request;
         }
 
-        public static String getSession(String[] response){
+        public static String getSess(String[] response){
+            //response holds sess info passed from server SESS version uniquesessionid
+            sessionID = response[2];
 
+            //make sure to prompt for creating, joining or looking at a list of open session ids
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.print("Would you like to \"create\" a game, \"join\" a game, or view a \"list\" of open games?");
+            String beginGame = scanner.nextLine();
+
+            if(beginGame.contains("create")) {
+                return "CREA " + clientIdentifier;
+            } else if(beginGame.contains("join")) {
+                System.out.print("Please enter the Game ID you'd like to join.");
+                String gameID = scanner.nextLine();
+                return "JOIN " + gameID;
+            } else if(beginGame.contains("list")) {
+                //add curr and all
+                System.out.print("Would you like to view all games \"currently\" open and in-play, or \"all\" games including those that have concluded?");
+                String listType = scanner.nextLine();
+
+                if(listType.contains("currently")) {
+                    return "LIST CURR";
+                } else if(listType.contains("all")) {
+                    return "LIST ALL";
+                }
+                return "";
+            } else {
+                return "";
+            }
+        }
+
+        public static String getGams(String[] response){
+            System.out.println("Here is a list of available games:");
+            for(int i = 1; i < response.length, i++) {
+                System.out.println(response[i]);
+            }
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Which game listed above would you like to join?");
+            String gameID = scanner.nextLine();
+
+            return "JOIN " + gameID;
+        }
+
+        public static String getJond(String[] response){
+            System.out.println("You have now joined the game: " + response[2]);
+            System.out.println("Please wait while we get the game started.");
+            //returns nothing because waiting for yrmv to be sent
+            return "";
+        }
+
+        public static String getYrmv(String[] response){
+            Scanner scanner = new Scanner(System.in);
+
+            //your move, game id, client whose turn it is id
+            if(response[2].equals(clientIdentifier)) {
+                System.out.println(board);
+                System.out.print("It is your turn to make a move, which space would you like to occupy?");
+                String moveSpace = scanner.nextLine();
+
+                return "MOVE " + response[1] + " " + moveSpace;
+            } else {
+                System.out.println(board);
+                System.out.println("It is not your move, please wait for player: " + response[2] + " to go.");
+                return "";
+            }
         }
 
     
